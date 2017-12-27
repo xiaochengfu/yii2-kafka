37859c5e84e2fb48e7f74a3fa5facdc641614111
xiaochengfu <1033426413@qq.com>
2017-12-27 22:22:59 +0800

    first commit

---
+++ controllers/ConsumerController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace xiaochengfu\kafka\controllers;
+
+
+use yii\console\Controller;
+
+/**
+ * RabbitMQ consumer functionality
+ * @package mikemadisonweb\rabbitmq\controllers
+ */
+class ConsumerController extends Controller
+{
+    public function actionStart(){
+        $setting = \Yii::$app->params['kafka'];
+        $conf = new RdKafka\Conf();
+        $conf->set('sasl.mechanisms', 'PLAIN');
+        $conf->set('api.version.request', 'true');
+        $conf->set('sasl.username', $setting['sasl_plain_username']);
+        $conf->set('sasl.password', $setting['sasl_plain_password']);
+        $conf->set('security.protocol', 'SASL_SSL');
+        $conf->set('ssl.ca.location', __DIR__ . '/ca-cert');
+
+        $conf->set('group.id', $setting['consumer_id']);
+
+        $conf->set('metadata.broker.list', $setting['bootstrap_servers']);
+
+        $topicConf = new RdKafka\TopicConf();
+
+        $topicConf->set('auto.offset.reset', 'smallest');
+
+        $conf->setDefaultTopicConf($topicConf);
+
+        $consumer = new RdKafka\KafkaConsumer($conf);
+
+        $consumer->subscribe([$setting['topic_name']]);
+
+        echo "Waiting for partition assignment... (make take some time when\n";
+        echo "quickly re-joining the group after leaving it.)\n";
+
+        while (true) {
+            $message = $consumer->consume(120 * 1000);
+            switch ($message->err) {
+                case RD_KAFKA_RESP_ERR_NO_ERROR:
+                    var_dump($message);
+                    break;
+                case RD_KAFKA_RESP_ERR__PARTITION_EOF:
+                    echo "No more messages; will wait for more\n";
+                    break;
+                case RD_KAFKA_RESP_ERR__TIMED_OUT:
+                    echo "Timed out\n";
+                    break;
+                default:
+                    throw new \Exception($message->errstr(), $message->err);
+                    break;
+            }
+        }
+    }
+}
